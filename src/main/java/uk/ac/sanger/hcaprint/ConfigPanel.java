4fe1fad9f58a76aabac29553c348934328ca6740
David Robinson <14000840+khelwood@users.noreply.github.com>
2019-06-21 15:45:49 +0100

    Deleted MapTableModel in favour of reusing FunctionalTableModel
    Turns out FunctionalTableModel works really well for this too.

--- src/main/java/uk/ac/sanger/hcaprint/ConfigPanel.java
+++ src/main/java/uk/ac/sanger/hcaprint/ConfigPanel.java
@@ -1,11 +1,10 @@
 package uk.ac.sanger.hcaprint;
 
 import javax.swing.*;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumnModel;
+import javax.swing.table.*;
 import java.awt.*;
-import java.util.*;
 import java.util.List;
+import java.util.*;
 
 /**
  * Panel for showing the application's config (in a modal dialog)
@@ -24,16 +23,21 @@ public class ConfigPanel extends JPanel {
     }
 
     private void initComponents() {
-        List<Object> propertyKeys = new ArrayList<>(properties.keySet());
-        propertyKeys.sort(Comparator.comparing(Object::toString));
-        MapTableModel<?, ?> model = new MapTableModel<>(properties, propertyKeys,
-                "Property", "Value");
-        table = new JTable(model);
+        table = new JTable(makeTableModel(properties));
         table.setFont(new Font(Font.MONOSPACED, Font.PLAIN, table.getFont().getSize()));
         table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
         sizeColumns(table);
         okButton = new JButton("OK");
         okButton.addActionListener(e -> okPressed());
+    }
+
+    private static TableModel makeTableModel(Map<?, ?> map) {
+        List<Map.Entry<?,?>> mapEntries = new ArrayList<>(map.entrySet());
+        mapEntries.sort(Comparator.comparing(e -> e.getKey().toString()));
+        FunctionalTableModel<Map.Entry<?, ?>> tableModel = new FunctionalTableModel<>(Map.Entry::getKey, Map.Entry::getValue);
+        tableModel.setHeadings("Property", "Value");
+        tableModel.setItems(mapEntries);
+        return tableModel;
     }
 
     private void sizeColumns(JTable table) {
